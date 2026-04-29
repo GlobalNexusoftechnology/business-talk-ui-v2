eae4b8d81feb52d701ca0d2b0cadd2eaa03a9371
Inamdar Mohammed Ishaque Mohammed Saeed <inamdarishaque8@gmail.com>
2026-04-29 18:40:31 +0530

    fixed UI

--- src/lib/roles.ts
+++ src/lib/roles.ts
@@ -1,6 +1,6 @@
 export const ROLES = {
-  ADMIN: '63765d88-d6de-4fd3-8528-a9de0107ba4d',
-  SUPER_ADMIN: 'SUPER_ADMIN_ID_HERE', // 🔥 replace with real one
+  ADMIN: 'bc6cd1f9-44b6-4688-b78c-94abbe97ae4a', // bc6cd1f9-44b6-4688-b78c-94abbe97ae4a
+  SUPER_ADMIN: '63765d88-d6de-4fd3-8528-a9de0107ba4d', // 🔥 replace with real one
 }
 
 export const isAdmin = (roleId?: string) => {
